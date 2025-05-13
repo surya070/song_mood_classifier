80747344f035a65952316614f09eb9696e860ad2
Surya Maheswaran <94157626+surya070@users.noreply.github.com>
2025-05-14 03:19:41 +0530

    Add files via upload

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,11 @@
+# flake8: noqa: E402
 import sys
 import os
 import pytest
-from app import app as flask_app
-
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
     )
+from app import app as flask_app
 
 
 @pytest.fixture
