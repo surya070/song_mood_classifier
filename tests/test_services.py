93a43ee0cb91f53d7550deac9a2622e9e76b2d91
Surya Maheswaran <94157626+surya070@users.noreply.github.com>
2025-05-13 23:22:33 +0530

    Add files via upload

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,6 +1,7 @@
 from unittest.mock import patch
 from services import spotify_service
 
+
 @patch("services.spotify_service.get_user_info")
 def test_get_user_info(mock_get_user_info):
     mock_get_user_info.return_value = "TestUser"
